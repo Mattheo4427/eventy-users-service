3d8d93169ffcdfd8a9c74c657caa4af71ec9c046
AlexisChartier <alexischartier30130pse@gmail.com>
2025-11-26 13:27:23 +0100

    fix init schema

--- src/main/resources/db/migration/V1__init_schema.sql
+++ src/main/resources/db/migration/V1__init_schema.sql
@@ -1,12 +1,25 @@
 -- Création de la table users
 CREATE TABLE users (
     user_id UUID PRIMARY KEY,
+    
+    username VARCHAR(255) NOT NULL UNIQUE,
     email VARCHAR(255) NOT NULL UNIQUE,
+    
     first_name VARCHAR(255) NOT NULL,
     last_name VARCHAR(255) NOT NULL,
-    balance DOUBLE PRECISION NOT NULL DEFAULT 0.0,
-    role VARCHAR(50) NOT NULL DEFAULT 'USER'
+    avatar_url VARCHAR(255),
+    
+    -- "date" est un mot clé SQL, l'utilisation des guillemets est recommandée pour éviter les conflits
+    "date" DATE NOT NULL,
+    
+    -- Stockage des Enums sous forme de chaîne
+    status VARCHAR(50) NOT NULL,
+    role VARCHAR(50) NOT NULL,
+    
+    -- Correspond à BigDecimal(precision=10, scale=2)
+    balance NUMERIC(10, 2) NOT NULL DEFAULT 0.00
 );
 
--- Index sur l'email pour optimiser la recherche
+-- Index pour optimiser les recherches
 CREATE INDEX idx_users_email ON users(email);
+CREATE INDEX idx_users_username ON users(username);
